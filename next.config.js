94cdb4ea7dfe4a6c3164127d5a29e325a2b66ec8
barpracajr-ai <barpracajr@gmail.com>
2026-08-19 21:51:14 -0300

    Set output to 'standalone' in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuração propositalmente vazia para evitar qualquer conflito de compatibilidade
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
